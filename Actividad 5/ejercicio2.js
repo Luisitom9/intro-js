cf2dee96361f5e9056e357ec0d18908eb657356e
Luisitom <luis.vazquez.mend@hotmail.com>
2022-05-04 17:30:32 -0500

    Subida de actividades

---
+++ Actividad 5/ejercicio2.js
@@ -0,0 +1,7 @@
+var arreglo= [];
+var pokemon;
+while (!(arreglo.length == 5)){
+    pokemon = prompt ('Mneciona 5 pokemon');
+    arreglo.push(pokemon)
+}
+console.log('Tus pokemon son:'+ arreglo);
---
+++ Actividad 6/ejercicio3.js
@@ -0,0 +1,5 @@
+var animals = ['eagle','parrot','monkey','boar','lion']
+var comingSoonAnimals = ['panter','dragon','turtle']
+var allAnimals = animals.concat(comingSoonAnimals);
+allAnimals[8]='cow'
+console.log(allAnimals);
